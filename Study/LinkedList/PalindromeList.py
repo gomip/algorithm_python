da748e82e22656883f9fcefb7761eba0a5203f3a
gomip <rlawjdgns15@hotmail.com>
2021-03-12 00:32:55 +0900

    linked list 확인

--- Study/LinkedList/PalindromeList.py
+++ Study/LinkedList/PalindromeList.py
@@ -4,68 +4,87 @@ import collections
 
 # 2021.03.10 | gomip | created
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val: int, next=None):
         self.val = val
-        self.next = next
+        self.next = None
 
 
-def palindrome_list(head: ListNode) -> bool:
-    # q: List = []
-    #
-    # if not head:
-    #     return True
-    #
-    # node = head
-    # while node is not None:
-    #     print('val', node.val)
-    #     q.append(node.val)
-    #     print('next', node.next)
-    #     node = node.next
-    #
-    # while len(q) > 1:
-    #     if q.pop(0) != q.pop():
-    #         return False
-    # return True
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.count = 0
 
-    # q: Deque = collections.deque
-    #
-    # if not head:
-    #     return True
-    #
-    # node = head
-    # if node is not None:
-    #     q.append(node.val)
-    #     node= node.next
-    #
-    # while len(q) > 1 :
-    #     if q.popleft() != q.pop():
-    #         return False
-    # return True
+    def add_list(self, value: List[int]):
+        for i in range(len(value)):
+            print('i: ',i,'|', value[i])
+            tmp = ListNode(value[i])
+            if self.head is None:
+                self.head = tmp
+            else:
+                cur = self.head
+                while cur is not None:
+                    print(value[i])
+                    cur = cur.next
+                cur.next = tmp
+        # if self.head is None:
+        #     self.head = value
+        # else:
+        #     cur = self.head
+        #     while cur is not None:
+        #         cur = cur.next
+        #     cur.next = value
 
-    rev = None
-    slow = fast = head
+    def palindrome_list(head: ListNode) -> bool:
+        # q: List = []
+        #
+        # if not head:
+        #     return True
+        #
+        # node = head
+        # while node is not None:
+        #     print('val', node.val)
+        #     q.append(node.val)
+        #     print('next', node.next)
+        #     node = node.next
+        #
+        # while len(q) > 1:
+        #     if q.pop(0) != q.pop():
+        #         return False
+        # return True
 
-    while fast and fast.next :
-        fast = fast.next.next
-        rev, rev.next, slow = slow, rev, slow.next
-    if fast :
-        slow = slow.next
+        # q: Deque = collections.deque
+        #
+        # if not head:
+        #     return True
+        #
+        # node = head
+        # if node is not None:
+        #     q.append(node.val)
+        #     node= node.next
+        #
+        # while len(q) > 1 :
+        #     if q.popleft() != q.pop():
+        #         return False
+        # return True
 
-    while rev and rev.val == slow.val:
-        slow, rev = slow.next, rev.next
-    return not rev
+        rev = None
+        slow = fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
+        return not rev
+
 
 if __name__ == '__main__':
-    head = ListNode()
-    node1 = ListNode(1)
-    node2 = ListNode(2)
-    node3 = ListNode(2)
-    node4 = ListNode(1)
-    head2 = ListNode()
+    head = LinkedList()
+    head.add_list([1, 2, 1])
+    # arr = [1, 2, 1]
 
-    head.val = node1
-    head.next = node2
-
-
-    res = palindrome_list(head)
-    print('res', res)
+    # res = palindrome_list(head)
+    # print('res', res)
