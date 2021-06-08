256ff7f5facd73e02bcc723d9ee63101aa1bf2bf
gomip <rlawjdgns15@hotmail.com>
2021-06-08 21:08:38 +0900

    dp 완료

---
+++ Study/Algorithm/Greedy/GastStation.py
@@ -0,0 +1,18 @@
+from typing import List
+import collections
+
+# 2021.06.06 | gomip | created
+
+class GasStation:
+    def solution(self, gas: List[int], cost: List[int]) -> int:
+        answer = 0
+        return answer
+    
+
+if __name__ == '__main__':
+    gas = [1, 2, 3, 4, 5]
+    cost = [3, 4, 5, 1, 2]
+
+    temp = GasStation()
+    ans = temp.solution(gas, cost)
+    print("ans", ans)
